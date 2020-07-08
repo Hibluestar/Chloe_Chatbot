0776840b24fc95b94e196a39c330388a8a624cdb
Hibluestar <67965712+Hibluestar@users.noreply.github.com>
2020-07-08 10:23:08 -0400

    Add files via upload

---
+++ Chloe's chatbot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+print('hello!')
+name=input('what is your name?')
+print(name,'is a magnificent name!')
+feel=input('how are you feeling today?')
+print(feel,'is not good!')
+color=input('what is your favorite color?')
+print(color,'is a dark color...')
+food=input('what is your favorite food? ')
+print(food,'yum!')
+subject=input('what is your favorite subject?')
+print(subject,'is fun!')
+print('well, it is nice to meet you',name)
+
